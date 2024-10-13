e91ff78db9a017d099bf1d7d6c2af171f94c6729
Anvin Antony <72195881+anvony@users.noreply.github.com>
2024-10-13 22:53:54 +0400

    Add files via upload

---
+++ anvin_drawing.py
@@ -0,0 +1,18 @@
+import turtle
+import re
+import pixart
+
+
+#runs the function to draw shape from entered text file.
+
+def main():
+    pixart.initialization(turtle)
+    pixart.draw_shape_from_file(turtle)
+    input()
+    
+
+if __name__ == "__main__":
+    main()
+
+
+    
